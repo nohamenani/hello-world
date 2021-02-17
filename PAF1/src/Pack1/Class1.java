f7672804c8612ceba40030b7f6490a0d06e86a16
Noha_Menani <nohamenani3107@gmail.com>
2021-02-17 11:20:22 +0530

    https://github.com/nohamenani/PAF_1.git

---
+++ PAF1/src/Pack1/Class1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Pack1;
+
+/**
+ * @author menani
+ *
+ */
+public class Class1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is for familiaring with git hub , git bash , git push activities under PAF");
+
+	}
+
+}
