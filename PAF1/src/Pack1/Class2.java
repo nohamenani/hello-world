568c7b8bfe6d9dcee4773db31dc0580f7408b3e4
Noha_Menani <nohamenani3107@gmail.com>
2021-03-18 00:32:12 +0530

    class2 java file added

--- PAF1/src/Pack1/Class2.java
+++ PAF1/src/Pack1/Class2.java
@@ -1,10 +1,8 @@
 package Pack1;
 
 public class Class2 {
-
-	public static void main(String[] args) {
-		System.out.println("This is the 2nd class that I have created here !");
-
+	
+		public static void main(String[] args) {
+			System.out.println("This is the 2nd class that I have created here !");
 	}
-
 }
